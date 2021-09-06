39e988a95dec9df261ea0b5a7cb7cad668337472
Miryam <mbathilde@gmail.com>
2021-09-06 13:14:32 +0200

    creación de componentes y servicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { Error404Component } from './components/error404/error404.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    Error404Component,
+    ProductListComponent,
+    DetailProductComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
