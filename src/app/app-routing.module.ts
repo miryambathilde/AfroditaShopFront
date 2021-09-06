1ce3fbd2c50bc3b02433a9ec2bc9fc040e3f48cd
Miryam <mbathilde@gmail.com>
2021-09-06 15:45:40 +0200

    Product service + Peticiones Http + GetAll de todos los productos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailProductComponent } from "./components/detail-product/detail-product.component";
-import { Error404Component } from "./components/error404/error404.component";
-import { HomeComponent } from "./components/home/home.component";
-import { ProductListComponent } from "./components/product-list/product-list.component";
+import { DetailProductComponent } from './components/detail-product/detail-product.component';
+import { Error404Component } from './components/error404/error404.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: 'home', component: HomeComponent},
-  { path: 'product-list', component: ProductListComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'products-list', component: ProductListComponent },
   { path: 'product/:idproduct', component: DetailProductComponent },
   { path: '**', component: Error404Component }
 ];
@@ -17,4 +17,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
