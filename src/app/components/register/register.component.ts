8dfe294177ee889f043f0ad897cba7159bb35ce8
Miryam <mbathilde@gmail.com>
2021-09-07 11:12:26 +0200

    formulario de registro e insertado en BBDD de nuevos users

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, Validators, FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from "src/app/services/users.service";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  formulario: FormGroup;
+  constructor(private router: Router, private usersService: UsersService) {
+    this.formulario = new FormGroup(
+      {
+        username: new FormControl('', [Validators.minLength(4)]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(8)
+        ]),
+        repite_password: new FormControl('', [])
+      },
+      [this.validadorPassword]
+    );
   }
 
+  ngOnInit() {}
+
+  validadorPassword(form: AbstractControl) {
+    const valorPassword = form.get('password')?.value;
+    const repitePassword = form.get('repite_password')?.value;
+    if (valorPassword === repitePassword) {
+      return null;
+    } else {
+      return { validadorPassword: true };
+    }
+  }
+
+  checkControl(controlName: string, errorName: string): boolean {
+    if (
+      this.formulario.get(controlName)?.hasError(errorName) &&
+      this.formulario.get(controlName)?.touched
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  async onSubmit() {
+    const result = await this.usersService.register(this.formulario.value);
+    console.log(result);
+    //console.log(this.formulario.value)
+  }
 }
